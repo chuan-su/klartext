0178ef59792e03d4ad145f146bb9ac8b9d08482e
Chuan Su <chuansu@icloud.com>
2017-05-21 00:03:42 +0200

    users resource post testing

--- src/main/java/se/klartext/app/controllers/UserController.java
+++ src/main/java/se/klartext/app/controllers/UserController.java
@@ -1,6 +1,7 @@
 package se.klartext.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,14 +12,24 @@ import se.klartext.app.models.UserRepository;
  * Created by suchuan on 2017-05-15.
  */
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping(value = "/users",method = RequestMethod.GET)
-    public @ResponseBody Iterable<User> greeting(){
+    @RequestMapping(method = RequestMethod.GET)
+    public Iterable<User> greeting(){
         return userRepository.findAll();
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @RequestMapping(value="/{id}",method = RequestMethod.GET)
+    public User getUserById(@PathVariable long id){
+        return userRepository.findOne(id);
+    }
 }
---
+++ src/test/java/se/klartext/app/controllers/UserControllerTest.java
@@ -0,0 +1,59 @@
+package se.klartext.app.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import se.klartext.app.KlartextApplication;
+import se.klartext.app.models.User;
+
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+/**
+ * Created by suchuan on 2017-05-20.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = KlartextApplication.class)
+@WebAppConfiguration
+public class UserControllerTest {
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .build();
+    }
+
+    @Test
+    public void createUser() throws Exception {
+
+        User user = new User("tony ivanov","tony@inserve.se","tony");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users")
+        .contentType(contentType)
+        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+    }
+
+}
+
