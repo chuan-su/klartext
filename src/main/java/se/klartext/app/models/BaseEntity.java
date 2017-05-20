a1e49466da86813189c5d593d3f0c1c0bcdc948a
Chuan Su <chuansu@icloud.com>
2017-05-21 00:04:06 +0200

    started using Lombok annotations

--- src/main/java/se/klartext/app/models/BaseEntity.java
+++ src/main/java/se/klartext/app/models/BaseEntity.java
@@ -1,7 +1,12 @@
 package se.klartext.app.models;
 
+import lombok.Data;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
@@ -10,16 +15,19 @@ import java.time.LocalDateTime;
 /**
  * Created by suchuan on 2017-05-20.
  */
+@Data
 @MappedSuperclass
-@Getter
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue
     Long id;
 
+    @CreatedDate
     LocalDateTime createdAt;
 
+    @LastModifiedDate
     LocalDateTime updatedAt;
 
 }
--- src/main/java/se/klartext/app/models/User.java
+++ src/main/java/se/klartext/app/models/User.java
@@ -1,6 +1,6 @@
 package se.klartext.app.models;
 
-import lombok.Getter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -9,10 +9,11 @@ import java.time.LocalDateTime;
  * Created by suchuan on 2017-05-20.
  */
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Table(name = "users")
 public class User extends BaseEntity{
 
-    String name,email,password;
-
+    private String name,email,password;
 }
