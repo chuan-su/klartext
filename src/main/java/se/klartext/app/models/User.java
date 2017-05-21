fc05de6621c2d946f1e6e55529e5bfbde56530a3
Chuan Su <chuansu@icloud.com>
2017-05-22 00:08:21 +0200

    jackson WRITE_ONLY access fro password field in User

--- src/main/java/se/klartext/app/models/User.java
+++ src/main/java/se/klartext/app/models/User.java
@@ -1,9 +1,13 @@
 package se.klartext.app.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by suchuan on 2017-05-20.
@@ -15,5 +19,12 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 public class User extends BaseEntity{
 
-    private String name,email,password;
+    private String name,email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private  String password;
+
+    @OneToMany(mappedBy = "createdBy")
+    @JsonIgnore
+    private List<Post> posts = new ArrayList<>();
 }
