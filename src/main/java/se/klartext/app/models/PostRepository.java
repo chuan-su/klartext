ffa7b5f0ac6dc5fe3eaa4d72636d1d8d38a2d65e
Chuan Su <chuansu@icloud.com>
2017-05-22 22:47:06 +0200

    use Java 8 optional and stream in Spring JPA

--- src/main/java/se/klartext/app/models/PostRepository.java
+++ src/main/java/se/klartext/app/models/PostRepository.java
@@ -1,12 +1,20 @@
 package se.klartext.app.models;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * Created by suchuan on 2017-05-21.
  */
-public interface PostRepository extends CrudRepository<Post,Long> {
-    Collection<Post> findByCreatedById(Long userId);
+public interface PostRepository extends Repository<Post,Long> {
+
+    Stream<Post> findByCreatedById(Long userId);
+
+    Optional<Post> findOne(Long id);
+
+    <T extends Post> T save(T post);
 }
