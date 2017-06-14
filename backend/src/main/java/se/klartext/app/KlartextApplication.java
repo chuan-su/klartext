ce60f608efb0a7757193f50c0df83e2dd1960705
Chuan Su <chuansu@icloud.com>
2017-06-15 00:23:25 +0200

    enable CORS

--- backend/src/main/java/se/klartext/app/KlartextApplication.java
+++ backend/src/main/java/se/klartext/app/KlartextApplication.java
@@ -3,7 +3,11 @@ package se.klartext.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EntityScan(
         basePackageClasses = {KlartextApplication.class, Jsr310JpaConverters.class}
@@ -14,4 +18,16 @@ public class KlartextApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KlartextApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+
 }
+
