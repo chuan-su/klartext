e653d5f61ac2ab6a3d159b9ce6bbb7568edaf0de
Chuan Su <chuan@Chuans-MBP.lan>
2017-06-21 00:28:41 +0200

    cleanup needed.fixed spring boot dev tool auto start with docker and remote debug.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,addr
 
 exec java $JAVA_OPTS \
      -Djava.security.egd=file:/dev/./urandom \
-     -jar /klartext/backend/backend-0.0.1-SNAPSHOT.jar
+     -jar /klartext/backend/build/libs/backend-0.0.1-SNAPSHOT.jar
