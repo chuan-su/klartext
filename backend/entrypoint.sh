41b19c9e95bbf7ac1ec32c3edca2639029edbd5f
Chuan Su <chuan@Chuans-MBP.lan>
2017-06-20 23:16:47 +0200

    added entrypoint.sh for dockerized spring boot app

---
+++ backend/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000"
+
+exec java $JAVA_OPTS \
+     -Djava.security.egd=file:/dev/./urandom \
+     -jar /klartext/backend/backend-0.0.1-SNAPSHOT.jar
