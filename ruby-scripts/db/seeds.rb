33492e9cde07e8624d1934e64bf25057731978fc
chuan-su <chuansu@icloud.com>
2017-07-11 16:18:29 +0200

    minor updates

--- ruby-scripts/db/seeds.rb
+++ ruby-scripts/db/seeds.rb
@@ -5,10 +5,3 @@ Klartext::User.find_or_create_by(name: 'chuan') do |user|
   user.email = 'chuan.su@outlook.com'
   user.password = 'China2017'
 end
-
-(0..20).each do |num|
-  Klartext::Post.find_or_create_by(body: "svenska_#{num}") do |post|
-    post.interp = "english_#{num}"
-    post.created_by = Klartext::User.first.id
-  end
-end
