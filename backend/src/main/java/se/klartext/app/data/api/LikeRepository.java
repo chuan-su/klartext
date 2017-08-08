b29b2b21c00c459af48b91cb57f1ee22c6b45af2
chuan-su <chuansu@icloud.com>
2017-08-08 13:30:50 +0200

    get post like count and user likes count

--- backend/src/main/java/se/klartext/app/data/api/LikeRepository.java
+++ backend/src/main/java/se/klartext/app/data/api/LikeRepository.java
@@ -1,6 +1,8 @@
 package se.klartext.app.data.api;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
 import se.klartext.app.model.Like;
 
@@ -10,5 +12,13 @@ public interface LikeRepository extends Repository<Like,Long> {
 
     Optional<Like> findByUserIdAndPostId(Long userId,Long postId);
 
+    Page<Like> findByPostId(Long postId, Pageable pageable);
+
+    Long countByPostId(Long postId);
+
+    Long countByUserId(Long userId);
+
     Like save(Like like);
+
+    void delete(Like like);
 }
--- backend/src/test/java/se/klartext/app/data/LikeRepositoryTest.java
+++ backend/src/test/java/se/klartext/app/data/LikeRepositoryTest.java
@@ -3,10 +3,12 @@ package se.klartext.app.data;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 import se.klartext.app.config.PersistenceConifg;
 import se.klartext.app.data.api.LikeRepository;
@@ -14,10 +16,11 @@ import se.klartext.app.model.Like;
 import se.klartext.app.model.Post;
 import se.klartext.app.model.User;
 
+import java.util.Arrays;
 import java.util.Optional;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -64,5 +67,92 @@ public class LikeRepositoryTest {
         assertTrue(likeEntity.isPresent());
         assertThat(likeEntity.get().getPost().getBody(),is(post.getBody()));
         assertThat(likeEntity.get().getUser().getEmail(),is(user.getEmail()));
+    }
+    @Test
+    public void testFindByPostId() throws Exception {
+
+        final User user = entityManager.persist(
+                User.builder().name("chuan").password("credentials").email("chuan@mail.se").build()
+        );
+
+        final Post post = entityManager.persist(
+                Post.builder().body("test1").interp("test interp1").createdBy(user).build()
+        );
+
+        User user1 = User.builder().name("testuser1").password("credentials1").email("testuser1@mail.se").build();
+        User user2 = User.builder().name("testuser2").password("credentials2").email("testuser2@mail.se").build();
+        User user3 = User.builder().name("testuser3").password("credentials3").email("testuser3@mail.se").build();
+
+        Arrays.asList(user1,user2,user3).stream()
+                .map(u -> entityManager.persist(u))
+                .map(u -> Like.builder().post(post).user(u).build())
+                .forEach(like -> entityManager.persist(like));
+
+        Pageable pageable = new PageRequest(0,10);
+
+        Page<Like> likes = likeRepo.findByPostId(post.getId(),pageable);
+        assertThat(likes,hasProperty("content"));
+        assertThat(likes.getTotalElements(),equalTo(Long.valueOf(3)));
+    }
+
+    @Test
+    public void testCountByPostId() throws Exception {
+
+        final User user = entityManager.persist(
+                User.builder().name("user").password("credentials").email("user@mail.se").build()
+        );
+
+        final Post post = entityManager.persist(
+                Post.builder().body("test1").interp("test interp1").createdBy(user).build()
+        );
+
+        entityManager.persist(Like.builder().post(post).user(user).build());
+
+        User user1 = User.builder().name("testuser1").password("credentials1").email("testuser1@mail.se").build();
+        User user2 = User.builder().name("testuser2").password("credentials2").email("testuser2@mail.se").build();
+        User user3 = User.builder().name("testuser3").password("credentials3").email("testuser3@mail.se").build();
+
+        final Post post2 = entityManager.persist(
+                Post.builder().body("test1").interp("test interp1").createdBy(user).build()
+        );
+
+        Arrays.asList(user1,user2,user3).stream()
+                .map(u -> entityManager.persist(u))
+                .map(u -> Like.builder().post(post2).user(u).build())
+                .forEach(like -> entityManager.persist(like));
+
+        Long userCount = likeRepo.countByPostId(post2.getId());
+
+        assertThat(userCount,equalTo(Long.valueOf(3)));
+    }
+
+    @Test
+    public void testCountByUserId() throws Exception {
+
+        final User user = entityManager.persist(
+                User.builder().name("user").password("credentials").email("user@mail.se").build()
+        );
+        final Post post = entityManager.persist(
+                Post.builder().body("test").interp("test interp").createdBy(user).build()
+        );
+
+        entityManager.persist(Like.builder().user(user).post(post).build());
+
+        Post post1 = Post.builder().body("test1").interp("test interp1").createdBy(user).build();
+        Post post2 = Post.builder().body("test1").interp("test interp1").createdBy(user).build();
+        Post post3 = Post.builder().body("test1").interp("test interp1").createdBy(user).build();
+
+        final User user2 = entityManager.persist(
+                User.builder().name("user2").password("credentials2").email("user2@mail.se").build()
+        );
+
+        Arrays.asList(post1,post2,post3).stream()
+                .map(p -> entityManager.persist(p))
+                .map(p -> Like.builder().post(p).user(user2).build())
+                .forEach(like -> entityManager.persist(like));
+
+        Long postCount = likeRepo.countByUserId(user2.getId());
+
+        assertThat(postCount,equalTo(Long.valueOf(3)));
     }
 }
