c05c49b532b11bc181697b4134d46044e4cca8e8
chuan-su <chuansu@icloud.com>
2017-08-10 22:31:54 +0200

    post resource api route refactor

--- backend/src/main/java/se/klartext/app/api/PostController.java
+++ backend/src/main/java/se/klartext/app/api/PostController.java
@@ -3,18 +3,24 @@ package se.klartext.app.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.DeferredResult;
 import se.klartext.app.business.api.PostService;
+import se.klartext.app.lib.exception.HttpUnauthorizedException;
 import se.klartext.app.model.Post;
+import se.klartext.app.model.User;
+import se.klartext.app.model.elasticsearch.PostDocument;
+import se.klartext.app.security.impl.UserDetailsImpl;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 /**
  * Created by suchuan on 2017-05-21.
  */
 @RestController
-@RequestMapping("/api/users/{userId}/posts")
 public class PostController {
 
     private final PostService postService;
@@ -25,29 +31,54 @@ public class PostController {
     }
 
     @Transactional
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/api/users/{userId}/posts",method = RequestMethod.GET)
     public Page<Post> getByAuthor(@PathVariable Long userId, Pageable pageable){
         return postService.findByAuthorId(userId,pageable);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public DeferredResult<Post> create(@PathVariable Long userId, @RequestBody Post post){
-        DeferredResult<Post> result = new DeferredResult<>();
-        postService.create(userId,post).subscribe(p -> result.setResult(p));
+    @RequestMapping(value = "/api/posts",method = RequestMethod.POST)
+    public DeferredResult<PostDocument> create(@RequestBody Post post){
+        User user = getUserFromSecurityContext();
+        post.setCreatedBy(user);
+        DeferredResult<PostDocument> result = new DeferredResult<>();
+        postService.create(post).subscribe(result::setResult);
         return result;
     }
 
-    @RequestMapping(value = "/{postId}",method = RequestMethod.PUT)
-    public DeferredResult<Post> update(@PathVariable Long postId,@RequestBody Post post){
-        DeferredResult<Post> result = new DeferredResult<>();
-        postService.update(postId,post).subscribe(p -> result.setResult(p));
+    @RequestMapping(value = "/api/posts/{postId}",method = RequestMethod.PUT)
+    public DeferredResult<PostDocument> update(@PathVariable Long postId,@RequestBody Post post){
+        DeferredResult<PostDocument> result = new DeferredResult<>();
+        postService.update(postId,post).subscribe(result::setResult);
         return result;
     }
 
-    @RequestMapping(value = "/{postId}",method = RequestMethod.DELETE)
-    public DeferredResult<Post> delete(@PathVariable Long postId){
-        DeferredResult<Post> result = new DeferredResult<>();
+    @RequestMapping(value = "/api/posts/{postId}",method = RequestMethod.DELETE)
+    public DeferredResult<PostDocument> delete(@PathVariable Long postId){
+        DeferredResult<PostDocument> result = new DeferredResult<>();
         postService.delete(postId).subscribe(p -> result.setResult(p));
         return result;
+    }
+
+    @RequestMapping(value = "/api/posts/{postId}/likes",method = RequestMethod.POST)
+    public DeferredResult<PostDocument> addLikes(@PathVariable Long postId){
+        User user = getUserFromSecurityContext();
+        DeferredResult<PostDocument> result = new DeferredResult<>();
+        postService.addLikes(postId,user).subscribe(result::setResult);
+        return result;
+    }
+
+    @RequestMapping(value = "/api/posts/{postId}/likes",method = RequestMethod.DELETE)
+    public DeferredResult<PostDocument> deleteLikes(@PathVariable Long postId){
+        User user = getUserFromSecurityContext();
+        DeferredResult<PostDocument> result = new DeferredResult<>();
+        postService.deleteLikes(postId,user).subscribe(result::setResult);
+        return result;
+    }
+
+    private User getUserFromSecurityContext(){
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return Optional.ofNullable(((UserDetailsImpl)userDetails).getUser())
+                .orElseThrow(() -> new HttpUnauthorizedException("authenticated user is required"));
     }
 }
